05536ba2c9eb3988935ad24e0e3dce73f7a38fa1
crescenton <crescentononline@gmail.com>
2026-05-19 18:22:33 +0530

    Delete pyislamic-prayer directory

--- pyislamic-prayer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .prayer_times import PrayerTimes
-from .madhab import Madhab
-from .methods import *
-from .qibla import qibla_direction
--- pyislamic-prayer/methods.py
+++
@@ -1,9 +0,0 @@
-class Method:
-    def __init__(self, fajr, isha):
-        self.fajr = fajr
-        self.isha = isha
-
-MWL = Method(18, 17)
-EGYPT = Method(19.5, 17.5)
-KARACHI = Method(18, 18)
-UMM_AL_QURA = Method(18.5, None)  # Isha = Maghrib + 90 min
